19c513b9bca92d0b05218b8d7c9ff6059175a3a0
Mario Leopold <mariole@edu.aau.at>
2020-04-07 16:03:41 +0200

    Grundlegendes Hauptmenü und Settings-Menü hinzugefügt

---
+++ app/src/main/java/com/example/cluedo_seii/SettingScreen.java
@@ -0,0 +1,16 @@
+package com.example.cluedo_seii;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ActionBar;
+import android.drm.DrmStore;
+import android.os.Bundle;
+
+public class SettingScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting_screen);
+    }
+}
