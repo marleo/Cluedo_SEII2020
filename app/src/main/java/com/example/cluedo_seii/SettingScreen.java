f2c16c4408de23dd17f2455b29424acfd7fdb7b0
Mario Leopold <mariole@edu.aau.at>
2020-04-07 17:57:39 +0200

    fixed main menu logo name error

--- app/src/main/java/com/example/cluedo_seii/SettingScreen.java
+++ app/src/main/java/com/example/cluedo_seii/SettingScreen.java
@@ -5,12 +5,29 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.ActionBar;
 import android.drm.DrmStore;
 import android.os.Bundle;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.ToggleButton;
 
 public class SettingScreen extends AppCompatActivity {
+
+    private static Boolean cheatEnabled;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting_screen);
+
+        final Switch cheatToggle = findViewById(R.id.cheatToggle);
+        cheatToggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                cheatEnabled = isChecked;
+            }
+        });
+    }
+
+    public static Boolean getCheatEnabled() {
+        return cheatEnabled;
     }
 }
