75e426123045131ffaa5d4378b00f5a14b29158a
marco9218 <marco.prinster@gmx.at>
2020-04-27 18:44:13 +0200

    Klasse Game Entwurf für Methode gameControl angelegt Klasse Player Attribute hinzugefügt, Konstruktor angelegt, getter Methoden angelegt und leere Methoden für accuse und suspect angelegt

--- app/src/main/java/com/example/cluedo_seii/Player.java
+++ app/src/main/java/com/example/cluedo_seii/Player.java
@@ -1,13 +1,39 @@
 package com.example.cluedo_seii;
 
 
+import com.example.cluedo_seii.spielbrett.GameboardElement;
+
 import java.util.LinkedList;
 
 public class Player {
 
     private int id;
     private LinkedList<Card> playerCards;
+    private GameboardElement position;
+    private String IP;
 
+    public Player(int id, GameboardElement position, String IP){
+        this.id = id;
+        playerCards = new LinkedList<>();
+        this.position = position;
+        this.IP = IP;
+    }
+
+    public GameboardElement getPosition() {
+        return position;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public LinkedList<Card> getPlayerCards() {
+        return playerCards;
+    }
+
+    public String getIP() {
+        return IP;
+    }
 
     public void addCard(Card card){
 
@@ -15,8 +41,11 @@ public class Player {
 
     }
 
+    public void suspect(){
 
+    }
 
+    public void accuse() {
 
-
+    }
 }
