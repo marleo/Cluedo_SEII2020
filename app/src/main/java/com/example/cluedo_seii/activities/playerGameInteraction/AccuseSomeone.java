34bce786cafb41ca449c6853c55ed9fcc0f67788
marco9218 <marco.prinster@gmx.at>
2020-05-05 18:12:35 +0200

    Aktivität Accuse Someone hinzugefügt

---
+++ app/src/main/java/com/example/cluedo_seii/activities/playerGameInteraction/AccuseSomeone.java
@@ -0,0 +1,16 @@
+package com.example.cluedo_seii.activities.playerGameInteraction;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.cluedo_seii.R;
+
+public class AccuseSomeone extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_accuse_someone);
+    }
+}
